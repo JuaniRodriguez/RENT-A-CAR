d916a41f89752809cbefd437ca6208e730acc049
juan ignacio <juani.rodriguez4444@gmail.com>
2023-01-30 21:26:20 -0300

    building crud

--- src/config/di.js
+++ src/config/di.js
@@ -2,6 +2,18 @@ const path=require('path');
 const multer=require('multer');
 const database=require('better-sqlite3')
 
+function uploadImages() {
+    const storage=multer.diskStorage({
+        destination:function(req,file,cb) {
+            cb(null,process.env.IMG_STORAGE)
+        },
+        filename:function(req,file,cb) {
+            cb(null, Date.now() + path.extname(file.originalname));
+        }       
+    })
+    return multer({storage:storage});
+}
+
 //function runDatabase() {
 //      return new database(process.env.DB_PATH,{verbose:console.log})
 //}
--- src/module/cars/controller/carsController.js
+++ src/module/cars/controller/carsController.js
@@ -4,6 +4,26 @@ export class carsController {
     constructor(carsService) {
         this.carsService=carsService
     }
+    //ver lo de upload
+    carRoutes(app) {
+        const baseRoute='/cars';
+        //post
+        app.get(`${baseRoute}/createCarForm`,this.createCar.bind(this));
+        app.post(`${baseRoute}/createCarForm`,this.createdCar.bind(this));
+        app.get(`${baseRoute}/editCar`,this.editCar.bind(this));
+        app.post(`${baseRoute}/editCar`,this.editedCar.bind(this))
+        app.get(`${baseRoute}/deleteCar`,this.deleteCar.bind(this))
+    }
+
+    async createCar(req,res) {
+        res.render('createCarForm.html')
+    }
+
+    async editCar(req,res) {
+        res.render('editCarForm.html')
+    }
+
+    
 
     //carRoutes(app) {
     //    //aca solo va un get al form, luego hago el post para crear el car
